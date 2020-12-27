37c06ac69eb1a937d48675a78eb53a76ec565276
hylas-develop <74232110+hylas-develop@users.noreply.github.com>
2020-12-28 00:02:50 +0900

    Edit and Delete Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import {
   deleteVideo,
-  editVideo,
+  getEditVideo,
   getUpload,
+  postEditVideo,
   postUpload,
   videoDetail,
 } from "../controllers/videoController";
@@ -11,12 +12,14 @@ import routes from "../routes";
 
 const videoRouter = express.Router();
 
-// videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+videoRouter.get(routes.deleteVideo(), deleteVideo);
+
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 export default videoRouter;
