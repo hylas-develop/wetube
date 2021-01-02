4db268a719bf8778e0463b7d3b305e156d157add
hylas-develop <74232110+hylas-develop@users.noreply.github.com>
2021-01-02 19:41:22 +0900

    Enter Fullscreen Exit Fullscreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,24 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayBtn");
 const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScrnBtn = document.getElementById("jsFullScreen");
+
+function exitFullScreen() {
+  fullScrnBtn.innerHTML = `<i class="fas fa-expand"></i>`;
+  fullScrnBtn.removeEventListener("click", exitFullScreen);
+  fullScrnBtn.addEventListener("click", goFullScreen);
+  //for Chrome
+  // Exit full screen function in document object
+  document.webkitExitFullscreen();
+}
+
+function goFullScreen() {
+  //for Chrome
+  videoContainer.webkitRequestFullscreen();
+  fullScrnBtn.innerHTML = `<i class="fas fa-compress"></i>`;
+  fullScrnBtn.removeEventListener("click", goFullScreen);
+  fullScrnBtn.addEventListener("click", exitFullScreen);
+}
 
 function handleVolumeClick() {
   if (videoPlayer.muted) {
@@ -26,6 +44,7 @@ function handlePlayClick() {
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
+  fullScrnBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
